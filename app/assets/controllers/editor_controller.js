0473b3077d53f74605fff65b07e750f4115f5f66
frandzdy <fsanon@webnet.fr>
2023-12-13 00:44:28 +0100

    Logo - checkin - checkout - refund

--- app/assets/controllers/editor_controller.js
+++ app/assets/controllers/editor_controller.js
@@ -1,10 +1,11 @@
-import { Controller } from '@hotwired/stimulus';
+import {Controller} from '@hotwired/stimulus';
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
 import List from '@ckeditor/ckeditor5-list/src/list';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
@@ -12,18 +13,20 @@ import TableProperties from '@ckeditor/ckeditor5-table/src/tableproperties';
 import TableCellProperties from '@ckeditor/ckeditor5-table/src/tablecellproperties';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
-import Image from '@ckeditor/ckeditor5-image/src/image';
-import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import FontBackgroundColor from '@ckeditor/ckeditor5-font/src/fontbackgroundcolor';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 import Link from '@ckeditor/ckeditor5-link/src/link';
-import { FontSize } from '@ckeditor/ckeditor5-font';
+import AutoLink from '@ckeditor/ckeditor5-link/src/autolink';
+import {FontSize, FontColor} from '@ckeditor/ckeditor5-font';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import TextTransformation from "@ckeditor/ckeditor5-typing/src/texttransformation";
+import Undo from "@ckeditor/ckeditor5-undo/src/undo";
+import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat";
 
 /**
- * Gestion des ckeditor
+ * Gestion des ckeditorlist-style-type
  * utiliser le data-controller="editor"
  *
- * Ajouter le data-enable-variables="true" pour gérer l'insertion des variables
  */
 export default class extends Controller {
     connect() {
@@ -33,33 +36,165 @@ export default class extends Controller {
                 plugins: [
                     Essentials, Autoformat, Bold, Italic, List, FontBackgroundColor, FontSize,
                     Table, TableToolbar, TableProperties, TableCellProperties,
-                    Heading, Link, Alignment, HorizontalLine
+                    Heading, Link, Alignment, HorizontalLine,
+                    BlockQuote, this.DisallowNestingBlockQuotes, Undo, FontColor, RemoveFormat, AutoLink, Underline,
+                    this.RemoveFormatLinks, TextTransformation, Underline
                 ],
-                toolbar: [
-                    'bold', 'italic', 'fontBackgroundColor', '|', 'alignment', '|', 'numberedList', 'bulletedList',
-                    '|', 'link', 'insertTable', '|', 'heading', '|', 'fontsize', '|',
-                    !!this.element.dataset.enableVariables ? 'insertVariable' : '|'
-                ],
+                toolbar: {
+                    items: [
+                        'undo', 'redo', '|',
+                        'heading', 'bold', 'italic', 'underline', '|',
+                        'fontColor', 'fontsize', '|',
+                        'alignment', 'blockQuote', 'removeformat', '|',
+                        'numberedList', 'bulletedList', '|',
+                        'link', 'insertTable'
+                    ],
+                    shouldNotGroupWhenFull: true
+                },
                 alignment: {
                     options: ['left', 'center', 'right']
                 },
                 fontSize: {
                     options: [
-                        8,
                         9,
-                        10,
                         11,
-                        12,
                         13,
-                        14,
-                        15,
                         'default',
-                        18,
-                        20
+                        17,
+                        19,
+                        21
                     ]
+                },
+                link: {
+                    decorators: {
+                        addTargetToExternalLinks: {
+                            mode: 'automatic',
+                            callback: url => /^(https?:)?\/\//.test( url ),
+                            attributes: {
+                                target: '_blank',
+                                rel: 'noopener noreferrer'
+                            }
+                        }
+                    }
                 },
                 table: {
                     contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells', 'tableProperties', 'tableCellProperties']
+                },
+                typing: {
+                    transformations: {
+                        // remove: [
+                        //     // Do not use the transformations from the
+                        //     // 'symbols' and 'quotes' groups.
+                        //     'symbols',
+                        //     'quotes',
+                        //
+                        //     // As well as the following transformations.
+                        //     'arrowLeft',
+                        //     'arrowRight'
+                        // ],
+                        extra: [
+                            // Add some custom transformations – e.g. for emojis.
+                            {from: ':)', to: '🙂'},
+                            {from: ':D', to: '😄'},
+                            {from: '<3', to: '❤️'},
+                            {from: ';)', to: '😉'},
+                            {from: ':P', to: '😛'},
+                            {from: ':*', to: '😘'},
+                            {from: ':(', to: '☹️'},
+                            {from: ':O', to: '😲'},
+                            {from: ':|', to: '😐'},
+                            {from: ':]', to: '😊'},
+                            {from: ':$', to: '🤑'},
+                            {from: ':]', to: '😇'},
+                            {from: ':$', to: '🤑'},
+                            {from: 'XD', to: '😆'},
+                            {from: ':B', to: '😎'},
+                            {from: ':*', to: '😗'},
+                            {from: ':\'(', to: '😢'},
+                            {from: ':\'D', to: '😂'},
+                            {from: ':fire:', to: '🔥'},
+                            {from: ':thumbsup:', to: '👍'},
+                            {from: ':thumbsdown:', to: '👎'},
+                            {from: ':muscle:', to: '💪'},
+                            {from: ':victory:', to: '✌️'},
+                            {from: ':pray:', to: '🙏'},
+                            {from: ':heart:', to: '❤️'},
+                            {from: ':star:', to: '⭐'},
+                            {from: ':rainbow:', to: '🌈'},
+                            {from: ':coffee:', to: '☕'},
+                            {from: ':pizza:', to: '🍕'},
+                            {from: ':taco:', to: '🌮'},
+                            {from: ':burrito:', to: '🌯'},
+                            {from: ':cake:', to: '🍰'},
+                            {from: ':wine:', to: '🍷'},
+                            {from: ':beer:', to: '🍺'},
+                            {from: ':cocktail:', to: '🍹'},
+                            {from: ':sushi:', to: '🍣'},
+                            {from: ':ramen:', to: '🍜'},
+                            {from: ':panda:', to: '🐼'},
+                            {from: ':koala:', to: '🐨'},
+                            {from: ':cat:', to: '🐱'},
+                            {from: ':dog:', to: '🐶'},
+                            {from: ':unicorn:', to: '🦄'},
+                            {from: ':alien:', to: '👽'},
+                            {from: ':ghost:', to: '👻'},
+                            {from: ':robot:', to: '🤖'},
+                            {from: ':zombie:', to: '🧟'},
+                            {from: ':clown:', to: '🤡'},
+                            {from: ':nerd:', to: '🤓'},
+                            {from: ':rockon:', to: '🤘'},
+                            {from: ':peace:', to: '✌️'},
+                            {from: ':ok:', to: '👌'},
+                            {from: ':sunglasses:', to: '😎'},
+                            {from: ':dizzy:', to: '💫'},
+                            {from: ':zap:', to: '⚡'},
+                            {from: ':bomb:', to: '💣'},
+                            {from: ':gun:', to: '🔫'},
+                            {from: ':rose:', to: '🌹'},
+                            {from: ':sunflower:', to: '🌻'},
+                            {from: ':tulip:', to: '🌷'},
+                            {from: ':cactus:', to: '🌵'},
+                            {from: ':moon:', to: '🌙'},
+                            {from: ':star2:', to: '🌟'},
+                            {from: ':cloud:', to: '☁️'},
+                            {from: ':rain:', to: '🌧️'},
+                            {from: ':snowflake:', to: '❄️'},
+                            {from: ':umbrella:', to: '☔'},
+                            {from: ':fireworks:', to: '🎆'},
+                            {from: ':balloon:', to: '🎈'},
+                            {from: ':gift:', to: '🎁'},
+                            {from: ':birthday:', to: '🎂'},
+                            {from: ':music:', to: '🎶'},
+                            {from: ':guitar:', to: '🎸'},
+                            {from: ':film:', to: '🎬'},
+                            {from: ':microphone:', to: '🎤'},
+                            {from: ':headphones:', to: '🎧'},
+                            {from: ':camera:', to: '📷'},
+                            {from: ':video:', to: '🎥'},
+                            {from: ':computer:', to: '💻'},
+                            {from: ':phone:', to: '📱'},
+                            {from: ':clock:', to: '🕰️'},
+                            {from: ':money:', to: '💰'},
+                            {from: ':thumbsup:', to: '👍'},
+                            {from: ':thumbsdown:', to: '👎'},
+
+                            // You can also define patterns using regular expressions.
+                            // Note: The pattern must end with `$` and all its fragments must be wrapped
+                            // with capturing groups.
+                            // The following rule replaces ` "foo"` with ` «foo»`.
+                            //{
+                            //  from: /(^|\s)(")([^"]*)(")$/,
+                            //  to: [ null, '«', null, '»' ]
+                            //},
+
+                            // Finally, you can define `to` as a callback.
+                            // This (naive) rule will auto-capitalize the first word after a period, question mark, or an exclamation mark.
+                            {
+                                from: /([.?!] )([a-z])$/,
+                                to: matches => [null, matches[1].toUpperCase()]
+                            }
+                        ],
+                    }
                 }
             })
             .then(editor => {
@@ -68,5 +203,20 @@ export default class extends Controller {
                 });
             })
         ;
+    }
+
+    DisallowNestingBlockQuotes = (editor) => {
+        editor.model.schema.addChildCheck((context, childDefinition) => {
+            if (context.endsWith('blockQuote') && childDefinition.name === 'blockQuote') {
+                return false;
+            }
+        });
+    }
+
+    RemoveFormatLinks = (editor) => {
+        // Extend the editor schema and mark the "linkHref" model attribute as formatting.
+        editor.model.schema.setAttributeProperties('linkHref', {
+            isFormatting: true
+        });
     }
 }
